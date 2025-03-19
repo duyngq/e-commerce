f59acc24ff086385c9c6d70b3d46962ecff72a96
duyng <lonelyemerald@gmail.com>
2025-03-19 20:27:26 +0700

    e16 - update document

--- src/main/java/com/store/exception/localization/LocalizationException.java
+++ src/main/java/com/store/exception/localization/LocalizationException.java
@@ -5,19 +5,17 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 @ToString
 @EqualsAndHashCode
 public class LocalizationException {
     private String errorCode;
     private String description;
-
-    public LocalizationException(){
-
-    }
 
     @JsonIgnore
     public LocalizationException cloneAndAppendDescription(String append){
